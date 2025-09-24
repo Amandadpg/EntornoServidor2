f735ff55ebbb147286669b4b9f9848bc22720691
Adlpg <delapridamanda1@gmail.com>
2025-09-24 10:06:28 +0200

    cositas

---
+++ ejemplos/id.php
@@ -0,0 +1,4 @@
+<?php
+// Accede al parámetro 'id' desde la URL
+echo $_GET['id'];  // Output: 123
+?>
---
+++ holamundo.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Prueba de PHP</title>
+</head>
+<body>
+    <?php phpinfo(); ?>
+</body>
+</html>
+
