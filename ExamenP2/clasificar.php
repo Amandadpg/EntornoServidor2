6f27b72ac054484dd8afa5ff133fffa7e48fcb7a
Adlpg <delapridamanda1@gmail.com>
2025-11-04 12:54:16 +0100

    ejercicios de cookies

--- ExamenP2/clasificar.php
+++ ExamenP2/clasificar.php
@@ -8,13 +8,17 @@
         <h1>Formulario: Lista de peliculas</h1>
         <p>Introduce al menos <b>4 peliculas</b> con titulo, año y duracion (min).</p>
         <?php
+        //Comprobamos que este vacio o no, con el isset
         if(isset($_POST["titulo"]) && isset($_POST["anio"]) && isset($_POST["duracion"])) {
+            //Recupero los datos del formulario
             $titulo = $_POST["titulo"];
             $anio = $_POST["anio"];
             $duracion = $_POST["duracion"];
+            //Declaro el array nuevo y categoria para la funcion
             $peliculas = [];
             $categoria = "";
 
+            //Voy recorriendo el array con cada una de las entidades
             for($i = 0; $i < 4; $i++) {
                 $peliculas[] = ["titulo" => $titulo[$i],
                                 "anio" => $anio[$i],
@@ -23,7 +27,7 @@
             }
 
 
-
+            //Realizo la funcion para determinar la categoria de cada pelicula
             function determinarCategoria($anio, $duracion) {
                 $categoria = "";
                 if($anio >= 2023 && $duracion > 120 ){
@@ -37,24 +41,26 @@
             return $categoria;
             }
 
-            if (4 > 0) {
-                echo "<table border ='1' >";
-                echo "<tr><th>Título</th><th>Año</th><th>Duración (min)</th><th>Categoría</th></tr>";
 
-                foreach ($peliculas as $peli) {
-                    $categoria = determinarCategoria($peli["anio"], $peli["duracion"]);
-                    echo "<tr>";
-                    echo "<td>{$peli['titulo']}</td>";
-                    echo "<td>{$peli['anio']}</td>";
-                    echo "<td>{$peli['duracion']}</td>";
-                    echo "<td class='categoria'>{$categoria}</td>";
-                    echo "</tr>";
-                }
+            //Empiezo a hacer la tabla que me pide
+            echo "<table border ='1' >";
+            echo "<tr><th>Título</th><th>Año</th><th>Duración (min)</th><th>Categoría</th></tr>";
 
-                echo "</table>";
+            //Recorro el array de pelicula para poder ir poniendolo en la tabla
+            foreach ($peliculas as $peli) {
+                $categoria = determinarCategoria($peli["anio"], $peli["duracion"]);
+                echo "<tr>";
+                echo "<td>{$peli['titulo']}</td>";
+                echo "<td>{$peli['anio']}</td>";
+                echo "<td>{$peli['duracion']}</td>";
+                echo "<td class='categoria'>{$categoria}</td>";
+                echo "</tr>";
             }
-
+            //Cierro la tabla
+            echo "</table>";
         }
+
+        
         ?>
     </body>
 </html>
